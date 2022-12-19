19c4d950c4bc869c7482952bcf378d3cbf1797ec
MattAlvero <matthewadrianoalvero@gmail.com>
2022-12-18 23:48:16 -0600

    filter css applied

--- src/stories/organisms/Filter/Filter.tsx
+++ src/stories/organisms/Filter/Filter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { InputLabel, MenuItem, FormControl, Select, Checkbox, OutlinedInput, ListItemText, Typography, FormGroup, FormControlLabel, Switch } from '@mui/material'
+import './filter.css';
+import { InputLabel, MenuItem, FormControl, Select, Checkbox, ListItemText, Typography, FormControlLabel, Switch } from '@mui/material'
 
 type FilterProps = {
     filters: string[],
@@ -11,45 +12,43 @@ export const Filter = ({filters}: FilterProps) => {
             <Typography>
                 Filters
             </Typography>
-            <FormGroup>
-                <FormControl>
-                    <InputLabel>{filters[0]}</InputLabel>
-                    <Select>
-                        <MenuItem key='Example' value='0'>
-                            <Checkbox checked={false} />
-                            <ListItemText primary='Example' />
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl>
-                    <InputLabel>{filters[1]}</InputLabel>
-                    <Select>
-                        <MenuItem key='Example' value='0'>
-                            <Checkbox checked={false} />
-                            <ListItemText primary='Example' />
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl>
-                    <InputLabel>{filters[2]}</InputLabel>
-                    <Select>
-                        <MenuItem key='Example' value='0'>
-                            <Checkbox checked={false} />
-                            <ListItemText primary='Example' />
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControl>
-                    <InputLabel>{filters[3]}</InputLabel>
-                    <Select>
-                        <MenuItem key='Example' value='0'>
-                            <Checkbox checked={false} />
-                            <ListItemText primary='Example' />
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-                <FormControlLabel control={<Switch defaultChecked/>} label='Sort By Recommened'></FormControlLabel>
-            </FormGroup>
+            <FormControl sx={{ width: 159}} variant='standard' >
+                <InputLabel>{filters[0]}</InputLabel>
+                <Select disableUnderline={true} >
+                    <MenuItem key='Example' value='0'>
+                        <Checkbox checked={false} />
+                        <ListItemText primary='Example' />
+                    </MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{ width: 159}} variant='standard' >
+                <InputLabel>{filters[1]}</InputLabel>
+                <Select disableUnderline={true} >
+                    <MenuItem key='Example' value='0'>
+                        <Checkbox checked={false} />
+                        <ListItemText primary='Example' />
+                    </MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{ width: 159}} variant='standard' >
+                <InputLabel>{filters[2]}</InputLabel>
+                <Select disableUnderline={true} >
+                    <MenuItem key='Example' value='0'>
+                        <Checkbox checked={false} />
+                        <ListItemText primary='Example' />
+                    </MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{ width: 159}} variant='standard' >
+                <InputLabel>{filters[3]}</InputLabel>
+                <Select disableUnderline={true} >
+                    <MenuItem key='Example' value='0'>
+                        <Checkbox checked={false} />
+                        <ListItemText primary='Example' />
+                    </MenuItem>
+                </Select>
+            </FormControl>
+            <FormControlLabel control={<Switch defaultChecked/>} label='Sort By Recommened'></FormControlLabel>
         </div>
     );
 };
