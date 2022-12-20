3f61fc47a239f1face23173bdaa275984ba2172b
MattAlvero <matthewadrianoalvero@gmail.com>
2022-12-19 19:53:16 -0600

    finished welcome page

--- src/stories/NCU Pages/Welcome Page/WelcomePage.tsx
+++ src/stories/NCU Pages/Welcome Page/WelcomePage.tsx
@@ -3,6 +3,7 @@ import { NCUHeader } from '../../organisms/Headers/NCUHeader';
 import { Filter } from '../../organisms/Filter/Filter';
 import { FeedItem } from '../../molecules/FeedItem/FeedItem';
 import { CompanyInfoBox } from '../../organisms/CompanyInfoBox/CompanyInfoBox';
+import './welcomepage.css';
 
 type WelcomePageProps = {
     user: string,
@@ -11,31 +12,35 @@ type WelcomePageProps = {
 export const WelcomePage = ({user}: WelcomePageProps ) => {
     return (
         <div>
-            <NCUHeader links={['Home','Messages','Profile','Notifications']}></NCUHeader>
-            <Filter filters={['Time', 'Engagement', 'Industry', 'Experience']}></Filter>
-            <div>
-                <div>
+            <div className='WelcomePage-Header'>
+                <NCUHeader links={['Home','Messages','Profile','Notifications']}></NCUHeader>
+                <Filter filters={['Time', 'Engagement', 'Industry', 'Experience']}></Filter>
+            </div>
+            <div className='WelcomePage-Body'>
+                <div className='WelcomePage-FeedContainer'>
                     <h4>Your Feed</h4>
-                    <FeedItem 
-                        imgSrc='https://via.placeholder.com/75' 
-                        feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
-                    <FeedItem 
-                        imgSrc='https://via.placeholder.com/75' 
-                        feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
-                    <FeedItem 
-                        imgSrc='https://via.placeholder.com/75' 
-                        feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
-                    <FeedItem 
-                        imgSrc='https://via.placeholder.com/75' 
-                        feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
-                    <FeedItem 
-                        imgSrc='https://via.placeholder.com/75' 
-                        feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
-                    <FeedItem 
-                        imgSrc='https://via.placeholder.com/75' 
-                        feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                    <div className='WelcomePage-Feed'>
+                        <FeedItem 
+                            imgSrc='https://via.placeholder.com/75' 
+                            feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                        <FeedItem 
+                            imgSrc='https://via.placeholder.com/75' 
+                            feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                        <FeedItem 
+                            imgSrc='https://via.placeholder.com/75' 
+                            feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                        <FeedItem 
+                            imgSrc='https://via.placeholder.com/75' 
+                            feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                        <FeedItem 
+                            imgSrc='https://via.placeholder.com/75' 
+                            feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                        <FeedItem 
+                            imgSrc='https://via.placeholder.com/75' 
+                            feedActivity='Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum?'></FeedItem>
+                    </div>
                 </div>
-                <div>
+                <div className='WelcomePage-CompInfoBox'>
                     <CompanyInfoBox 
                         companyName='Company Name' 
                         companyDesc='What are you passionate about? Focus on what’s important to you in your daily life.'
